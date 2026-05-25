10f136b2fd013cc4dbf99c7c986dbf3f260791d7
JunhaoWang <1594260677@qq.com>
2026-05-25 16:55:06 +0800

    "fix clean code"
    Signed-off-by: JunhaoWang <1594260677@qq.com>

--- tests/ut/attention/test_sfa_cp.py
+++ tests/ut/attention/test_sfa_cp.py
@@ -90,7 +90,7 @@ class TestAscendSFACPMetadataBuilder(TestBase):
 
         self.mock_cfg.speculative_config = None
 
-        self.patcher = patchx(
+        self.patcher = patch(
             "vllm.config.get_current_vllm_config",
             return_value=self.mock_cfg,
         )
